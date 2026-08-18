1ccea6d40421294d4f69ca42e541943a8875506d
Yan Nguyen <nguyen.dtd@gmail.com>
2026-08-18 10:34:39 +0700

    Initial App

---
+++ app/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            molestias et aliquam omnis officiis incidunt eveniet nobis, numquam
+            vel qui! Voluptatem ipsam
+            <a
+                href="http://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                >asperiores
+            </a>
+            blanditiis officiis fuga aperiam est, qui quo.
+        </p>
+    </div>
+</template>
+
+<style lang="scss">
+@use '~/assets/styles/app.scss';
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: true },
+
+    modules: [
+        '@nuxt/content',
+        '@nuxt/eslint',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@vueuse/nuxt',
+        'nuxt-gtag'
+    ],
+
+    content: {
+        experimental: { nativeSqlite: true }
+    }
+});

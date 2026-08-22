5e3044ccaa4bbb25ab776ce97d444243e0a6d98b
Yan Nguyen <nguyen.dtd@gmail.com>
2026-08-22 18:20:41 +0700

    added placeholder

--- app/app.vue
+++ app/app.vue
@@ -11,10 +11,10 @@ useHead({
 
 <template>
     <NuxtLayout>
-        <div class="flex flex-col min-h-screen px-4 w-full max-w-6xl mx-auto">
+        <div class="flex flex-col min-h-screen px-4 w-full max-w-2xl mx-auto">
             <Header />
             <div class="container mx-auto flex-grow pb-2 mb-2">
-                <NuxtPage class="w-full max-w-2xl mx-auto" />
+                <NuxtPage class="w-full mx-auto" />
             </div>
             <Footer />
         </div>

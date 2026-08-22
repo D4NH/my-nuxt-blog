c05e6f673a8a3839dd75c1704b5e37e2feece6a9
Yan Nguyen <nguyen.dtd@gmail.com>
2026-08-22 15:52:27 +0700

    remove bootstrap

--- app/assets/plugins/bootstrap.client.ts
+++
@@ -1,6 +0,0 @@
-// plugins/bootstrap.client.ts
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-export default defineNuxtPlugin(() => {
-    // Bootstrap JS is initialized globally on window
-});
